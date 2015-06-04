9fb2ff8355083b9b8e3c527ca61c07cd97179811
deepikajovy <deepajovy@gmail.com>
2015-06-04 21:48:21 +1200

    connected all

--- connect4/index.js
+++ connect4/index.js
@@ -1,54 +1,44 @@
-$(document).ready(function() {
+var count = 0;
+
+$(document).ready(function () {
 
   var connect = new Connect();
-  connect.generateGrid();
- 
-  // connect.renderGrid();
-var count=0;
-$('div').click(function(){
-//var col = this.Find('id');
-//console.log(this)
-var colid = $(this).attr("class")
+  connect.generateGrid(); 
 
-console.log("colid:"+colid)
+    $('div').click(function () {
+     
+      var colid = $(this).attr("class")
+      console.log(colid);
 
-var col = $('.'+colid);
+      var col = $('.' + colid);
+      console.log(col); 
 
-//console.log(col)
-count++;
-if(count%2===1)
-{
-	//1st player
-	$( this ).addClass( "player1" );
-}
-else
-{
-	$( this ).addClass( "player2" );
-}
+      for (var i = 5; i >= 0; i--) {
 
-// console.log(this)
-var cell = $(col[0]);
+        console.log(col[i]);
+
+        if (!$(col[i]).hasClass("player1") && !$(col[i]).hasClass("player2")) {
+
+          count++;
+          if (count % 2 === 1) {
+            //1st player
+            $(col[i]).addClass("player1");
+            break;
+          }
+          else {
+            $(col[i]).addClass("player2");
+            break;
+          }
+        }
+      }
+
+    });
 
 
-for (var i = 5; i > 0; i--) {
- 
-if($('col'+ [i]).hasClass("player1"))
-{console.log(col[i])
+
+
+
+
    
-     $('col'+ [i]).addClass('player2')
-     break;
-}
-else
-{
-    console.log("hello there", col[i])
-    console.log("this is i", i)
-}
-
-}
-// console.log(col[i])
-$('col'+ [6]).addClass("player1")
-console.log("Added", col[6])
-console.log("this", this)
-
 });
-});
+
